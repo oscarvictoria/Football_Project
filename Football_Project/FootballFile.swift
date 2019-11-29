d736609880b46fb7ce639af02e0b5fa4f1faf5b1
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-11-29 16:25:15 -0500

    Initial Commit

---
+++ Football_Project/FootballFile.swift
@@ -0,0 +1,11 @@
+//
+//  FootballFile.swift
+//  Football_Project
+//
+//  Created by Oscar Victoria Gonzalez  on 11/29/19.
+//  Copyright © 2019 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import Foundation
+
+
